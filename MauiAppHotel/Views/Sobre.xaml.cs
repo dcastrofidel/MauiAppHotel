7ea5dc958f1e4445aeac3a0823072fa33149d317
dcastrofidel <dgarcia.fidel@gmail.com>
2024-11-10 21:49:10 -0300

    atualização dos layouts, do design, e "back-end" da parte da contratação da hospedagem, definição das diárias

--- MauiAppHotel/Views/Sobre.xaml.cs
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -6,4 +6,19 @@ public partial class Sobre : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PopAsync();
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "Ok");
+
+        }
+    }
 }
